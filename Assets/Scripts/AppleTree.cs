4f261ced58de992e69458af3ed6dca4eddfb2710
novakvova <novakvova@gmail.com>
2020-04-25 11:02:52 +0300

    Go Apple tree

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    //Шаблон для створення яблука
+    public GameObject applePrefab;
+    //Швидкість руху 
+    public float speed = 1f;
+
+    //Відстань на яку повинно змінюватися напрямок руху яблуні
+    public float leftAndRightEnge = 10f;
+
+    //Імовірність случайної зміни напряку руху
+    public float changeToChangeDirections = 0.1f;
+
+    //Частота створення екземплярів яблук
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Просте переміщення
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+    }
+}
