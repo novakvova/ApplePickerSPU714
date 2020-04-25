268fb0d16928d9db68c4f454b49b00879995cebe
novakvova <novakvova@gmail.com>
2020-04-25 11:49:35 +0300

    move basker mouse

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -13,6 +13,19 @@ public class Basket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Отримати поточні координати мишки на екрані з Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        //Координати Z камери визначаючий, як далеко в трохмірному просторі
+        //знаходиться вказівник миші
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        //Перетворити точку на двухмірній площині єкрану в трохмірні коодинати ігри
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //Переміщаємо корзину вздовж осі X в координату X вказівника миші
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
     }
 }
